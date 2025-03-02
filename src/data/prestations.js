f18cf345f74bf2ec33f420f31f804713ec39a47e
MohammedBohi <116156394+MohammedBohi@users.noreply.github.com>
2025-03-02 06:27:08 +0100

    erreur à resoudre ...

--- src/data/prestations.js
+++ src/data/prestations.js
@@ -1,5 +1,5 @@
 export default [
-    { id: 1, nom: "Coupe étudiante", prix: 15, image: require("@/assets/images/coupeEtudiante.jpeg") },
+    { id: 1, nom: "Coupe étudiante", prix: 1, image: require("@/assets/images/coupeEtudiante.jpeg") },
     { id: 2, nom: "Coupe homme", prix: 19, image: require("@/assets/images/coupeHomme.jpeg") },
     { id: 3, nom: "Coupe tondeuse", prix: 12, image: require("@/assets/images/coupeTondeuse.jpeg") },
     { id: 4, nom: "Rasage barbe à l'ancienne", prix: 15, image: require("@/assets/images/RasageBarbeAncienne.jpeg") },
