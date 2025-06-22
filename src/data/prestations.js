3b47fc42e62906a8a00a04b3b74bc12c7e8bf7ec
MohammedBohi <116156394+MohammedBohi@users.noreply.github.com>
2025-06-22 23:05:25 +0100

    modif des prix temporaires

--- src/data/prestations.js
+++ src/data/prestations.js
@@ -5,7 +5,7 @@ export default [
     { id: 4, nom: "Rasage barbe à l'ancienne", prix: 15, image: require("@/assets/images/RasageBarbeAncienne.jpeg") },
     { id: 5, nom: "Coupe enfant", prix: 13, image: require("@/assets/images/CoupeEnfant.jpeg") },
     { id: 6, nom: "Barbe tondeuse", prix: 10, image: require("@/assets/images/BarbeTondeuse.jpeg") },
-    { id: 7, nom: "Coupe homme + Barbe tondeuse ", prix: 29, image: require("@/assets/images/DoublePrestation.jpeg") },
+    { id: 7, nom: "Coupe homme + Barbe tondeuse ", prix: 30, image: require("@/assets/images/DoublePrestation.jpeg") },
     { id: 8, nom: "Coupe homme + Barbe rasage à l’ancienne", prix: 34, image: require("@/assets/images/Derniere.jpeg") },
 
   ];
